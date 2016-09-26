707a97810cbb7084ac6f8c9ff7b78a0e9aead08d
TheBigBlueBlob <liamszczepiorkowski@hanovernorwichschools.org>
2016-09-26 13:47:25 -0400

    Updated from Brython Server: 9/26/2016 1:47:25 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Liam S
-Credit: none
+Credit: Vinzent
 
 Assignment:
 
@@ -9,14 +9,29 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+encrypt = False
+decrypt = False
+answered = True
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 request = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-if request == "e":
-    print("encrypt")
-elif request == "d":
-    print("decrypt")
-elif request == "q":
-    print("quit")
-else:
-    print("Did not understand command, try again.")
+while answered == True:
+    if request == "e":
+        print("encrypt")
+        answered = False
+        encrypt = True
+        message = str(input("Message: "))
+        key = str(input("Key:"))
+    elif request == "d":
+        print("decrypt")
+        answered = False
+        decrypt = True
+    elif request == "q":
+        print("Goodbye!")
+        answered = False
+    else:
+        print("Did not understand command, try again.")
+
+
+
+
     
